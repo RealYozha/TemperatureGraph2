87576ddbeeb16f111f85b8a6c993784050e6b293
Yozha <130782610+RealYozha@users.noreply.github.com>
2025-09-08 17:23:44 +0300

    1.1.0-2

--- daterange.py
+++
@@ -1,7 +0,0 @@
-class DateRange:
-    def __init__(self, start_date: str, end_date: str) -> None:
-        self.start = start_date
-        self.end = end_date
-    
-    def unpack(self) -> tuple[str, str]:
-        return self.start, self.end
